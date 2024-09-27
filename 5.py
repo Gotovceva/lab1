f2eb60f73d573f725ccc22439cfbfd1390f916d5
Gotovceva <gotovtseva.angelina@gmail.com>
2024-09-27 17:40:38 +0300

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,18 @@
+# Лабараторная работа № 2
+# Задание 12, уровень 2
+#
+r = int(input())
+v = int(input())
+if v == 1:
+    s = r ** 2
+    print(s)
+else:
+    if v == 2:
+        s = r ** 2 * 3.14
+        print(s)
+    else:
+        if v == 3:
+            s = ((r ** 2) * (3 ** 0.5))/4
+            print(s)
+            
+    
