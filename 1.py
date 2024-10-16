12fad3757e12fa367162b163a3922b51a022d04c
Gotovceva <gotovtseva.angelina@gmail.com>
2024-10-16 18:25:19 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,33 @@
 # Лабараторная работа № 1
 # 8 задание 1 уровень
 # Вычислить s=1!+2!+...+6!
-x = 0
+s = 0
+x = 1
 for i in range(1,7):
-    y = 1
-    for q in range(1,i+1):
-        y = y * q
-    x += y
-print(x)
+    x = x*i
+    s += x
+print(s)
+
+# 1 уровень задание 4
+№ Вычислить s = cosx+cos2x/x+cos3x/x^2+...+cos9x/x^8
+import math
+s = 0
+x = int(input())
+for n in range(1,10):
+    i = math.cos(n*x)/(x**(n-1))
+    s += i
+print(s)
+
+# 1 уровень задание 9
+# Вычислить s = (-1)^1*5^1/1!+(-1)^2*5^2/2!+...+(-1)^6*5^6/6!
+s = 0
+a = 1
+x = 5
+for i in range(1,7):
+    a = a * i
+    t = ((-1)**i)*(x**i)/a
+    s += t
+print(s)
+
+
+
